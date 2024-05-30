5d475618ac8486b87b338dacd293d2f6b4228c06
pomdtr <achille.lacoin@gmail.com>
2024-05-30 22:31:56 +0200

    update example

--- examples/http/mod.ts
+++ examples/http/mod.ts
@@ -1,7 +1,5 @@
-export default {
-  fetch(_req: Request) {
-    return new Response("Hello from my macbook", {
-      headers: { "content-type": "text/plain" },
-    });
-  },
-};
+export default function () {
+  return new Response("Hello from my macbook", {
+    headers: { "content-type": "text/plain" },
+  });
+}
