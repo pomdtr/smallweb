8eb99f57516ba9da95e860e1afe4dd58a44adc70
pomdtr <achille.lacoin@gmail.com>
2024-07-05 18:51:06 +0200

    add plugin mechanism

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
 	"github.com/spf13/cobra"
 )
 
@@ -18,5 +24,55 @@ func NewCmdRoot(version string) *cobra.Command {
 	cmd.AddCommand(NewCmdCreate())
 	cmd.AddCommand(NewCmdUpgrade())
 
+	path := os.Getenv("PATH")
+	commands := make(map[string]*cobra.Command)
+	for _, dir := range filepath.SplitList(path) {
+		entries, err := os.ReadDir(dir)
+		if err != nil {
+			continue
+		}
+
+		for _, entry := range entries {
+			if strings.HasPrefix(entry.Name(), "smallweb-") {
+				entrypoint := filepath.Join(dir, entry.Name())
+				// check if the entrypoint is executable
+				if _, err := os.Stat(entrypoint); err != nil {
+					continue
+				}
+
+				if ok, err := isExecutable(entrypoint); !ok || err != nil {
+					continue
+				}
+
+				name := strings.TrimPrefix(entry.Name(), "smallweb-")
+				commands[name] = &cobra.Command{
+					Use:                name,
+					SilenceUsage:       true,
+					Short:              fmt.Sprintf("Extension %s", name),
+					DisableFlagParsing: true,
+					RunE: func(cmd *cobra.Command, args []string) error {
+						command := exec.Command(entrypoint, args...)
+						command.Stdin = os.Stdin
+						command.Stdout = os.Stdout
+						command.Stderr = os.Stderr
+						return command.Run()
+					},
+				}
+			}
+		}
+	}
+
+	for _, command := range commands {
+		cmd.AddCommand(command)
+	}
+
 	return cmd
+}
+
+func isExecutable(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	return fileInfo.Mode().Perm()&0111 != 0, nil
 }
