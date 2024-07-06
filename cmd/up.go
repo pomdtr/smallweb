29859e6b3e5cfc4f6aa758b0668f0924b684a9c8
pomdtr <achille.lacoin@gmail.com>
2024-07-06 16:09:56 +0200

    add support for serving tls

--- cmd/up.go
+++ cmd/up.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"net/http"
 	"os"
@@ -10,30 +12,98 @@ import (
 )
 
 func NewCmdUp() *cobra.Command {
+	var flags struct {
+		host          string
+		port          int
+		tls           bool
+		tlsCert       string
+		tlsKey        string
+		tlsCaCert     string
+		tlsClientAuth string
+	}
+
 	cmd := &cobra.Command{
 		Use:     "up",
 		Short:   "Start the smallweb evaluation server",
 		Aliases: []string{"serve"},
 		Args:    cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			port, _ := cmd.Flags().GetInt("port")
+			port := flags.port
+			if flags.port == 0 && flags.tls {
+				port = 443
+			} else if flags.port == 0 {
+				port = 7777
+			}
+			addr := fmt.Sprintf("%s:%d", flags.host, port)
+
 			server := http.Server{
-				Addr: fmt.Sprintf(":%d", port),
+				Addr: addr,
 				Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 					handler, err := worker.WorkerFromHost(r.Host)
 					if err != nil {
 						w.WriteHeader(http.StatusNotFound)
 						return
 					}
-
 					handler.ServeHTTP(w, r)
 				}),
 			}
 
-			fmt.Fprintln(os.Stderr, "Listening on", server.Addr)
+			if flags.tls {
+				if flags.tlsCert == "" || flags.tlsKey == "" {
+					return fmt.Errorf("TLS enabled, but no certificate or key provided")
+				}
+
+				cert, err := tls.LoadX509KeyPair(flags.tlsCert, flags.tlsKey)
+				if err != nil {
+					return fmt.Errorf("failed to load TLS certificate and key: %w", err)
+				}
+
+				tlsConfig := &tls.Config{
+					Certificates: []tls.Certificate{cert},
+					MinVersion:   tls.VersionTLS12,
+				}
+
+				if flags.tlsCaCert != "" {
+					caCert, err := os.ReadFile(flags.tlsCaCert)
+					if err != nil {
+						return fmt.Errorf("failed to read CA certificate: %w", err)
+					}
+					caCertPool := x509.NewCertPool()
+					if !caCertPool.AppendCertsFromPEM(caCert) {
+						return fmt.Errorf("failed to parse CA certificate")
+					}
+					tlsConfig.ClientCAs = caCertPool
+
+					switch flags.tlsClientAuth {
+					case "require":
+						tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+					case "request":
+						tlsConfig.ClientAuth = tls.RequestClientCert
+					case "verify":
+						tlsConfig.ClientAuth = tls.VerifyClientCertIfGiven
+					default:
+						return fmt.Errorf("invalid client auth option: %s", flags.tlsClientAuth)
+					}
+				}
+
+				server.TLSConfig = tlsConfig
+
+				cmd.Printf("Listening on https://%s\n", addr)
+				return server.ListenAndServeTLS(flags.tlsCert, flags.tlsKey)
+			}
+
+			cmd.Printf("Listening on http://%s\n", addr)
 			return server.ListenAndServe()
 		},
 	}
-	cmd.Flags().IntP("port", "p", 7777, "Port to listen on")
+
+	cmd.Flags().StringVar(&flags.host, "host", "localhost", "Host to listen on")
+	cmd.Flags().IntVarP(&flags.port, "port", "p", 0, "Port to listen on")
+	cmd.Flags().BoolVar(&flags.tls, "tls", false, "Enable TLS")
+	cmd.Flags().StringVar(&flags.tlsCert, "tls-cert", "", "TLS certificate file path")
+	cmd.Flags().StringVar(&flags.tlsKey, "tls-key", "", "TLS key file path")
+	cmd.Flags().StringVar(&flags.tlsCaCert, "tls-ca-cert", "", "TLS CA certificate file path")
+	cmd.Flags().StringVar(&flags.tlsClientAuth, "tls-client-auth", "require", "TLS client auth mode (require, request, verify)")
+
 	return cmd
 }
