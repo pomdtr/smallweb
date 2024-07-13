a5bab0b31b27612f712ed72af9353114f2c44596
pomdtr <achille.lacoin@gmail.com>
2024-07-13 11:46:41 +0200

    add interactive prompts for smallweb create

--- cmd/create.go
+++ cmd/create.go
@@ -1,15 +1,50 @@
 package cmd
 
 import (
-	"path"
+	"fmt"
 
+	"github.com/charmbracelet/huh"
 	"github.com/pomdtr/smallweb/templates"
-	"github.com/pomdtr/smallweb/worker"
 	"github.com/spf13/cobra"
 )
 
+type Inputs struct {
+	Name     string
+	Template string
+}
+
+func (i *Inputs) Fill() error {
+	var fields []huh.Field
+
+	if i.Name == "" {
+		fields = append(fields, huh.NewInput().Title("Choose a name").Value(&i.Name))
+	}
+
+	if i.Template == "" {
+		templates, err := templates.List()
+		if err != nil {
+			return fmt.Errorf("failed to list templates: %w", err)
+		}
+
+		var options []huh.Option[string]
+		for _, template := range templates {
+			options = append(options, huh.NewOption(template, template))
+		}
+
+		fields = append(fields, huh.NewSelect[string]().Title("Choose a template").Options(options...).Value(&i.Template))
+	}
+
+	if len(fields) == 0 {
+		return nil
+	}
+
+	form := huh.NewForm(huh.NewGroup(fields...))
+	return form.Run()
+}
+
 func NewCmdCreate() *cobra.Command {
 	var flags struct {
+		name     string
 		template string
 	}
 
@@ -17,13 +52,22 @@ func NewCmdCreate() *cobra.Command {
 		Use:     "create <app>",
 		Short:   "Create a new smallweb app",
 		GroupID: CoreGroupID,
-		Args:    cobra.ExactArgs(1),
+		Args:    cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			dst := path.Join(worker.SMALLWEB_ROOT, args[0])
-			return templates.Install(flags.template, dst)
+			inputs := Inputs{
+				Name:     flags.name,
+				Template: flags.template,
+			}
+
+			if err := inputs.Fill(); err != nil {
+				return fmt.Errorf("failed to fill inputs: %w", err)
+			}
+
+			return templates.Install(inputs.Template, inputs.Name)
 		},
 	}
 
+	cmd.Flags().StringVarP(&flags.name, "name", "n", "", "The name of the app")
 	cmd.Flags().StringVarP(&flags.template, "template", "t", "", "The template to use")
 	cmd.RegisterFlagCompletionFunc("template", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		options, err := templates.List()
@@ -32,7 +76,6 @@ func NewCmdCreate() *cobra.Command {
 		}
 		return options, cobra.ShellCompDirectiveNoFileComp
 	})
-	cmd.MarkFlagRequired("template")
 
 	return cmd
 }
