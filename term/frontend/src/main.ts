edca87514691aec0b873a883de53db8442f2dcb3
pomdtr <achille.lacoin@gmail.com>
2024-09-06 20:43:19 +0200

    update terminal message

--- term/frontend/src/main.ts
+++ term/frontend/src/main.ts
@@ -1,102 +1,105 @@
 import { Terminal } from "@xterm/xterm";
-import { FitAddon } from "@xterm/addon-fit"
-import { WebglAddon } from "@xterm/addon-webgl"
-import { WebLinksAddon } from "@xterm/addon-web-links"
-import { AttachAddon } from "@xterm/addon-attach"
+import { FitAddon } from "@xterm/addon-fit";
+import { WebglAddon } from "@xterm/addon-webgl";
+import { WebLinksAddon } from "@xterm/addon-web-links";
+import { AttachAddon } from "@xterm/addon-attach";
 import { nanoid } from "nanoid";
 
+const terminal = new Terminal({
+  cursorBlink: true,
+  allowProposedApi: true,
+  macOptionIsMeta: true,
+  macOptionClickForcesSelection: true,
+  fontSize: 13,
+  fontFamily: "Consolas,Liberation Mono,Menlo,Courier,monospace",
+  // theme: window.matchMedia("(prefers-color-scheme: dark)").matches
+  //   ? darkTheme
+  //   : lightTheme,
+});
 
-async function main() {
-  const terminal = new Terminal({
-    cursorBlink: true,
-    allowProposedApi: true,
-    macOptionIsMeta: true,
-    macOptionClickForcesSelection: true,
-    fontSize: 13,
-    fontFamily: "Consolas,Liberation Mono,Menlo,Courier,monospace",
-    // theme: window.matchMedia("(prefers-color-scheme: dark)").matches
-    //   ? darkTheme
-    //   : lightTheme,
-  });
+const webLinksAddon = new WebLinksAddon(
+  (event, uri) => {
+    // check if cmd key is pressed
+    if (event.metaKey || event.ctrlKey) {
+      window.open(uri, "_blank");
+    }
 
-  const webLinksAddon = new WebLinksAddon(
-    (event, uri) => {
-      // check if cmd key is pressed
-      if (event.metaKey || event.ctrlKey) {
-        window.open(uri, "_blank");
-      }
+    window.open(uri, "_self");
+  },
+);
+terminal.loadAddon(webLinksAddon);
 
-      window.open(uri, "_self");
-    },
-  );
-  terminal.loadAddon(webLinksAddon);
+terminal.loadAddon(new WebglAddon());
 
-  terminal.loadAddon(new WebglAddon());
+const fitAddon = new FitAddon();
+terminal.loadAddon(fitAddon);
 
-  const fitAddon = new FitAddon();
-  terminal.loadAddon(fitAddon);
+terminal.open(document.getElementById("terminal")!);
+fitAddon.fit();
 
-  terminal.open(document.getElementById("terminal")!);
+document.title = "Smalleb - Terminal";
+const terminalID = nanoid();
+
+const protocol = new URL(window.location.href).protocol;
+const websocketUrl = new URL(
+  window.location.href.replace(
+    protocol,
+    protocol === "https:" ? "wss:" : "ws:",
+  ),
+);
+
+websocketUrl.searchParams.set(
+  "_payload",
+  JSON.stringify({
+    id: terminalID,
+    cols: terminal.cols,
+    rows: terminal.rows,
+  }),
+);
+
+const ws = new WebSocket(websocketUrl.toString());
+
+window.onresize = () => {
   fitAddon.fit();
+};
 
-  document.title = "Smalleb - Terminal";
-  const terminalID = nanoid();
+terminal.onTitleChange((title) => {
+  document.title = title;
+});
 
-  const protocol = new URL(window.location.href).protocol
-  const websocketUrl = new URL(window.location.href.replace(protocol, protocol === "https:" ? "wss:" : "ws:"));
+terminal.onResize((size) => {
+  const { cols, rows } = size;
+  const url = new URL(window.location.href);
+  url.searchParams.set("cols", cols.toString());
+  url.searchParams.set("rows", rows.toString());
 
-  websocketUrl.searchParams.set("_payload", JSON.stringify({ id: terminalID, cols: terminal.cols, rows: terminal.rows }));
+  fetch(window.location.href, {
+    method: "PATCH",
+    body: JSON.stringify({ cols, rows, id: terminalID }),
+  });
+});
 
-  const ws = new WebSocket(websocketUrl.toString());
+const attachAddon = new AttachAddon(ws);
+terminal.loadAddon(attachAddon);
 
-  window.onresize = () => {
-    fitAddon.fit();
-  };
-
-  terminal.onTitleChange((title) => {
-    document.title = title;
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", function () {
+    // terminal.options.theme = e.matches ? darkTheme : lightTheme;
   });
 
-  terminal.onResize((size) => {
-    const { cols, rows } = size;
-    const url = new URL(window.location.href);
-    url.searchParams.set("cols", cols.toString());
-    url.searchParams.set("rows", rows.toString());
+ws.onclose = () => {
+  attachAddon.dispose();
+  terminal.write("\r\nConnection closed, you can now close this tab.\r\n");
+};
 
-    fetch(window.location.href, {
-      method: "PATCH",
-      body: JSON.stringify({ cols, rows, id: terminalID }),
-    });
-  });
+window.onbeforeunload = () => {
+  ws.onclose = () => {};
+  ws.close();
+};
 
-  const attachAddon = new AttachAddon(ws);
-  terminal.loadAddon(attachAddon);
-
-  window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", function () {
-      // terminal.options.theme = e.matches ? darkTheme : lightTheme;
-    });
-
-  ws.onclose = () => {
-    attachAddon.dispose()
-    terminal.write("\r\nConnection closed, press any key to close the terminal.\r\n");
-    terminal.onData(() => {
-      window.close();
-    });
-  }
-
-  window.onbeforeunload = () => {
-    ws.onclose = () => { };
-    ws.close();
-  };
-
-
-  window.onfocus = () => {
-    terminal.focus();
-  };
-
+window.onfocus = () => {
   terminal.focus();
-}
+};
 
-main();
+terminal.focus();
