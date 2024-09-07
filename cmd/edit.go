782d99bdb23533be78e17642e3cb2318658e540a
pomdtr <achille.lacoin@gmail.com>
2024-09-07 14:12:41 +0200

    add ability to edit a specific file

--- cmd/edit.go
+++ cmd/edit.go
@@ -18,13 +18,37 @@ func NewCmdEdit() *cobra.Command {
 	}
 
 	cmd := &cobra.Command{
-		Use:     "edit <app>",
+		Use:     "edit <app> [file]",
 		Short:   "Open an app in your editor",
 		GroupID: CoreGroupID,
-		Args:    cobra.ExactArgs(1),
+		Args:    cobra.RangeArgs(1, 2),
 		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 			rootDir := utils.ExpandTilde(k.String("dir"))
-			return ListApps(rootDir), cobra.ShellCompDirectiveNoFileComp
+			if len(args) == 0 {
+				return ListApps(rootDir), cobra.ShellCompDirectiveNoFileComp
+			}
+
+			// complete files in app directory
+			if len(args) == 1 {
+				appDir := filepath.Join(rootDir, args[0])
+				if utils.FileExists(appDir) {
+					entries, err := os.ReadDir(appDir)
+					if err != nil {
+						return nil, cobra.ShellCompDirectiveError
+					}
+
+					var files []string
+					for _, entry := range entries {
+						if !entry.IsDir() {
+							files = append(files, entry.Name())
+						}
+					}
+
+					return files, cobra.ShellCompDirectiveNoFileComp
+				}
+			}
+
+			return nil, cobra.ShellCompDirectiveNoFileComp
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			rootDir := utils.ExpandTilde(k.String("dir"))
@@ -37,22 +61,27 @@ func NewCmdEdit() *cobra.Command {
 						return fmt.Errorf("failed to load app: %v", err)
 					}
 
-					entrypoint := a.Entrypoint()
+					var entrypoint string
+					if len(args) == 2 {
+						entrypoint = filepath.Join(a.Dir, args[1])
+					} else {
+						entrypoint = a.Entrypoint()
 
-					if !utils.FileExists(entrypoint) {
-						if utils.FileExists(filepath.Join(a.Root(), "index.html")) {
-							entrypoint = filepath.Join(a.Root(), "index.html")
-						} else {
-							entries, err := os.ReadDir(a.Root())
-							if err != nil {
-								return fmt.Errorf("failed to read directory: %v", err)
+						if !utils.FileExists(entrypoint) {
+							if utils.FileExists(filepath.Join(a.Root(), "index.html")) {
+								entrypoint = filepath.Join(a.Root(), "index.html")
+							} else {
+								entries, err := os.ReadDir(a.Root())
+								if err != nil {
+									return fmt.Errorf("failed to read directory: %v", err)
+								}
+
+								if len(entries) == 0 {
+									return fmt.Errorf("no files in app directory")
+								}
+
+								entrypoint = filepath.Join(a.Root(), entries[0].Name())
 							}
-
-							if len(entries) == 0 {
-								return fmt.Errorf("no files in app directory")
-							}
-
-							entrypoint = filepath.Join(a.Root(), entries[0].Name())
 						}
 					}
 
