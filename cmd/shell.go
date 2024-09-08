1878ba1d4be1094ec5fe547f1dc7ee7655653c8e
pomdtr <achille.lacoin@gmail.com>
2024-09-08 20:43:03 +0200

    add proper prompt

--- cmd/shell.go
+++ cmd/shell.go
@@ -20,7 +20,7 @@ func NewCmdShell() *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			rootDir := utils.ExpandTilde(k.String("dir"))
 			shell := ishell.NewWithConfig(&readline.Config{
-				Prompt: "\033[32m$\033[0m ",
+				Prompt: fmt.Sprintf("\033[32m%s\033[0m $ ", k.String("domain")),
 			})
 
 			for _, appname := range ListApps(rootDir) {
