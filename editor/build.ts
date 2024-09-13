7329ba017e67becbb170ced3b3af2571d9373cc5
pomdtr <achille.lacoin@gmail.com>
2024-09-13 16:25:51 +0200

    remove editor

--- editor/build.ts
+++
@@ -1,9 +0,0 @@
-import { cpSync, existsSync, rmSync } from "node:fs";
-
-if (existsSync("dist")) {
-    rmSync("dist", { recursive: true });
-}
-
-cpSync("node_modules/vscode-web/dist", "dist", { recursive: true });
-cpSync("index.html", "dist/index.html");
-cpSync("product.json", "dist/product.json");
