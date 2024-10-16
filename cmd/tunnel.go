234a90aa2f153c05c1a060564e55f9929cac26d3
pomdtr <achille.lacoin@gmail.com>
2024-10-16 20:52:03 +0200

    simplify ssh tunnel command

--- cmd/tunnel.go
+++ cmd/tunnel.go
@@ -2,21 +2,18 @@ package cmd
 
 import (
 	"fmt"
-	"io"
-	"log"
 	"log/slog"
 	"net"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
-	"time"
 
 	"github.com/pomdtr/smallweb/api"
 	"github.com/pomdtr/smallweb/app"
 	"github.com/pomdtr/smallweb/database"
 	"github.com/pomdtr/smallweb/utils"
 	"github.com/spf13/cobra"
-	"golang.org/x/crypto/ssh"
 )
 
 func NewCmdTunnel() *cobra.Command {
@@ -27,15 +24,6 @@ func NewCmdTunnel() *cobra.Command {
 		Args:              cobra.ExactArgs(1),
 		ValidArgsFunction: completeApp(utils.ExpandTilde(k.String("dir"))),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			client, err := ssh.Dial("tcp", "localhost.run:22", &ssh.ClientConfig{
-				User:            "nokey",
-				HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-				Timeout:         5 * time.Second,
-			})
-			if err != nil {
-				return fmt.Errorf("failed to dial: %v", err)
-			}
-
 			db, err := database.OpenDB(filepath.Join(DataDir(), "smallweb.db"))
 			if err != nil {
 				return fmt.Errorf("failed to open database: %v", err)
@@ -65,65 +53,24 @@ func NewCmdTunnel() *cobra.Command {
 				return fmt.Errorf("failed to listen: %v", err)
 			}
 
-			go func() {
-				_, _, err := client.SendRequest("tcpip-forward", true, ssh.Marshal(remoteForwardRequest{
-					BindPort: 80,
-				}))
-				if err != nil {
-					log.Printf("failed to request forward: %v", err)
-					return
-				}
-
-				channels := client.HandleChannelOpen("forwarded-tcpip")
-
-				for channel := range channels {
-					ch, reqs, err := channel.Accept()
-					if err != nil {
-						log.Printf("failed to accept: %v", err)
-						continue
-					}
-
-					go ssh.DiscardRequests(reqs)
-
-					c, err := net.Dial("tcp", ln.Addr().String())
-					if err != nil {
-						ch.Close()
-						log.Printf("failed to dial: %v", err)
-						continue
-					}
-
-					go func() {
-						defer ch.Close()
-						defer c.Close()
-
-						io.Copy(c, ch)
-					}()
-
-					go func() {
-						defer c.Close()
-						defer ch.Close()
-
-						io.Copy(ch, c)
-					}()
-				}
-			}()
-
-			session, err := client.NewSession()
+			_, port, err := net.SplitHostPort(ln.Addr().String())
 			if err != nil {
-				return fmt.Errorf("failed to create session: %v", err)
+				return fmt.Errorf("failed to parse address: %v", err)
 			}
 
-			session.Stdin = os.Stdin
-			session.Stdout = os.Stdout
-			session.Stderr = os.Stderr
+			sshCommand := exec.Command("ssh", "-R", fmt.Sprintf("80:localhost:%s", port), "nokey@localhost.run")
 
-			if err := session.Shell(); err != nil {
-				return fmt.Errorf("failed to start shell: %v", err)
-			}
+			sshCommand.Stdin = os.Stdin
+			sshCommand.Stdout = os.Stdout
+			sshCommand.Stderr = os.Stderr
 
 			go server.Serve(ln)
 
-			return session.Wait()
+			if err := sshCommand.Run(); err != nil {
+				return fmt.Errorf("failed to start ssh command: %v", err)
+			}
+
+			return nil
 		},
 	}
 
