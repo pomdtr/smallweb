ad085f25bb055671669ab069d760a4313607a32f
pomdtr <achille.lacoin@gmail.com>
2024-11-15 18:37:01 +0100

    remove hidden watch command

--- cmd/root.go
+++ cmd/root.go
@@ -126,7 +126,6 @@ func NewCmdRoot(changelog string) *cobra.Command {
 	cmd.AddCommand(NewCmdCron())
 	cmd.AddCommand(NewCmdLogs())
 	cmd.AddCommand(NewCmdSync())
-	cmd.AddCommand(NewCmdWatch())
 
 	cmd.AddCommand(&cobra.Command{
 		Use:   "changelog",
