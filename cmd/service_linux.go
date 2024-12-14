3500aef3c898e0d7b3055d7dbec00811feced3e7
pomdtr <achille.lacoin@gmail.com>
2024-12-14 16:22:37 +0100

    remove root support in smallweb

--- cmd/service_linux.go
+++ cmd/service_linux.go
@@ -20,17 +20,12 @@ import (
 var serviceConfigBytes []byte
 var serviceConfig = template.Must(template.New("service").Parse(string(serviceConfigBytes)))
 
-func getServicePath(uid int) string {
-	if uid == 0 {
-		return path.Join("/etc", "systemd", "system", "smallweb.service")
-	}
-
+func getServicePath() string {
 	return path.Join(os.Getenv("HOME"), ".config", "systemd", "user", "smallweb.service")
 }
 
 func InstallService(args []string) error {
-	uid := os.Getuid()
-	servicePath := getServicePath(uid)
+	servicePath := getServicePath()
 	if utils.FileExists(servicePath) {
 		return fmt.Errorf("service already installed")
 	}
@@ -64,22 +59,6 @@ func InstallService(args []string) error {
 		return fmt.Errorf("failed to write service file: %v", err)
 	}
 
-	if uid == 0 {
-		if err := exec.Command("systemctl", "daemon-reload").Run(); err != nil {
-			return fmt.Errorf("failed to reload systemd manager configuration: %v", err)
-		}
-
-		if err := exec.Command("systemctl", "enable", "smallweb").Run(); err != nil {
-			return fmt.Errorf("failed to enable service: %v", err)
-		}
-
-		if err := exec.Command("systemctl", "start", "smallweb").Run(); err != nil {
-			return fmt.Errorf("failed to start service: %v", err)
-		}
-
-		return nil
-	}
-
 	// Reload the systemd manager configuration
 	if err := exec.Command("systemctl", "--user", "daemon-reload").Run(); err != nil {
 		return fmt.Errorf("failed to reload systemd manager configuration: %v", err)
@@ -99,18 +78,9 @@ func InstallService(args []string) error {
 }
 
 func StartService() error {
-	uid := os.Getuid()
-	servicePath := getServicePath(uid)
+	servicePath := getServicePath()
 	if !utils.FileExists(servicePath) {
 		return fmt.Errorf("service not installed")
-	}
-
-	if uid == 0 {
-		if err := exec.Command("systemctl", "start", "smallweb").Run(); err != nil {
-			return fmt.Errorf("failed to start service: %v", err)
-		}
-
-		return nil
 	}
 
 	if err := exec.Command("systemctl", "--user", "start", "smallweb").Run(); err != nil {
@@ -121,18 +91,9 @@ func StartService() error {
 }
 
 func StopService() error {
-	uid := os.Getuid()
-	servicePath := getServicePath(uid)
+	servicePath := getServicePath()
 	if !utils.FileExists(servicePath) {
 		return fmt.Errorf("service not installed")
-	}
-
-	if uid == 0 {
-		if err := exec.Command("systemctl", "stop", "smallweb").Run(); err != nil {
-			return fmt.Errorf("failed to stop service: %v", err)
-		}
-
-		return nil
 	}
 
 	if err := exec.Command("systemctl", "--user", "stop", "smallweb").Run(); err != nil {
@@ -143,18 +104,9 @@ func StopService() error {
 }
 
 func RestartService() error {
-	uid := os.Getuid()
-	servicePath := getServicePath(uid)
+	servicePath := getServicePath()
 	if !utils.FileExists(servicePath) {
 		return fmt.Errorf("service not installed")
-	}
-
-	if uid == 0 {
-		if err := exec.Command("systemctl", "restart", "smallweb").Run(); err != nil {
-			return fmt.Errorf("failed to restart service: %v", err)
-		}
-
-		return nil
 	}
 
 	if err := exec.Command("systemctl", "--user", "restart", "smallweb").Run(); err != nil {
@@ -165,30 +117,9 @@ func RestartService() error {
 }
 
 func UninstallService() error {
-	uid := os.Getuid()
-	servicePath := getServicePath(uid)
+	servicePath := getServicePath()
 	if !utils.FileExists(servicePath) {
 		return fmt.Errorf("service not installed")
-	}
-
-	if uid == 0 {
-		if err := exec.Command("systemctl", "stop", "smallweb").Run(); err != nil {
-			return fmt.Errorf("failed to stop service: %v", err)
-		}
-
-		if err := exec.Command("systemctl", "disable", "smallweb").Run(); err != nil {
-			return fmt.Errorf("failed to disable service: %v", err)
-		}
-
-		if err := os.Remove(servicePath); err != nil {
-			return fmt.Errorf("failed to remove service file: %v", err)
-		}
-
-		if err := exec.Command("systemctl", "daemon-reload").Run(); err != nil {
-			return fmt.Errorf("failed to reload systemd manager configuration: %v", err)
-		}
-
-		return nil
 	}
 
 	// Stop the service if it is running
@@ -214,23 +145,6 @@ func UninstallService() error {
 }
 
 func PrintServiceLogs(follow bool) error {
-	uid := os.Getuid()
-	if uid == 0 {
-		logCmdArg := []string{"smallweb"}
-		if follow {
-			logCmdArg = append(logCmdArg, "-f")
-		}
-
-		logCmd := exec.Command("journalctl", logCmdArg...)
-		logCmd.Stdout = os.Stdout
-		logCmd.Stderr = os.Stderr
-		if err := logCmd.Run(); err != nil {
-			return fmt.Errorf("failed to execute journalctl: %v", err)
-		}
-
-		return nil
-	}
-
 	logCmdArgs := []string{"--user", "--user-unit", "smallweb"}
 	if follow {
 		logCmdArgs = append(logCmdArgs, "-f")
@@ -247,18 +161,6 @@ func PrintServiceLogs(follow bool) error {
 }
 
 func ViewServiceStatus() error {
-	uid := os.Getuid()
-
-	if uid == 0 {
-		statusCmd := exec.Command("systemctl", "status", "smallweb")
-		statusCmd.Stdout = os.Stdout
-		statusCmd.Stderr = os.Stderr
-		if err := statusCmd.Run(); err != nil {
-			return fmt.Errorf("failed to get service status: %v", err)
-		}
-		return nil
-	}
-
 	statusCmd := exec.Command("systemctl", "--user", "status", "smallweb")
 	statusCmd.Stdout = os.Stdout
 	statusCmd.Stderr = os.Stderr
