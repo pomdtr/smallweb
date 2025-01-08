092094c5387dc3898e42dade87a8c36d028c9194
pomdtr <achille.lacoin@gmail.com>
2025-01-08 18:58:35 +0100

    add back deno dir env

--- worker/worker.go
+++ worker/worker.go
@@ -74,6 +74,7 @@ func commandEnv(a app.App, rootDir string, domain string) []string {
 
 	env = append(env, fmt.Sprintf("HOME=%s", os.Getenv("HOME")))
 	env = append(env, "DENO_NO_UPDATE_CHECK=1")
+	env = append(env, "DENO_DIR=%s", filepath.Join(xdg.CacheHome, "smallweb", "deno"))
 
 	env = append(env, fmt.Sprintf("SMALLWEB_VERSION=%s", build.Version))
 	env = append(env, fmt.Sprintf("SMALLWEB_DIR=%s", rootDir))
