1f2d0a329272078a0f88946fa57aace5cc90a45c
pomdtr <achille.lacoin@gmail.com>
2025-01-19 17:27:53 +0100

    feat: ensure data directory exists before saving email

--- examples/inbox/main.ts
+++ examples/inbox/main.ts
@@ -1,7 +1,9 @@
 import PostalMime from 'npm:postal-mime';
+import { ensureDir } from "jsr:@std/fs"
 
 export default {
     async email(input: ReadableStream<Uint8Array>) {
+        await ensureDir("data");
         const email = await PostalMime.parse(input);
         await Deno.writeTextFile("data/email.json", JSON.stringify(email, null, 4));
     }
