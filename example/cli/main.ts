16f1c91c8ac8d889a1f70a0e6a8c911839f6b0c3
pomdtr <achille.lacoin@gmail.com>
2025-05-09 19:35:25 +0200

    update cli example

--- example/cli/main.ts
+++ example/cli/main.ts
@@ -1,5 +1,17 @@
+import { cli } from 'npm:gunshi@0.17.0'
+
 export default {
-    run: () => {
-        console.log("Hello World");
-    },
-};
+    async run(args: string[]) {
+        await cli(args, {
+            options: {
+                name: {
+                    type: "string",
+                    default: "world",
+                }
+            },
+            run: (ctx) => {
+                console.log(`Hello ${ctx.values.name}!`)
+            }
+        })
+    }
+}
